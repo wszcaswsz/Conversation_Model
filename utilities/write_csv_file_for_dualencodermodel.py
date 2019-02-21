a489fec398f61a366a1910264a235c15ef7dd73d
wszcaswsz <47872043+wszcaswsz@users.noreply.github.com>
2019-02-21 15:39:16 -0800

    clean up

--- utilities/write_csv_file_for_dualencodermodel.py
+++ utilities/write_csv_file_for_dualencodermodel.py
@@ -29,48 +29,9 @@ import numpy as np
 
 
 
-def read_file(inputfile):
-    labels = []
-    texts = []
-    f = open(inputfile, 'r')
-    k = 0
-    r = 0
-    record = set()
-    skip = 0
-    for line in f:
-        # print(line)
-        k += 1
-        if k == 1:
-            continue
 
-        split_line = line.rstrip().replace('"', '').replace('\xa0', '  ').split(",")
-        cat = split_line[-1]
-        text = ','.join(split_line[:len(split_line) - 1])
-        # print (cat)
-        # print (text)
-        if (cat, text) not in record:
-            if cat != '00163E3CD6251EE7B7E962A036702F73':
-                record.add((cat, text))
-                texts.append(text)
-                labels.append(cat)
-                r += 1
-            else:
-                skip += 1
-        else:
-            skip += 1
-    print('read in total lines')
-    print(r)
-    print('skip duplicated lines')
-    print(skip)
-    f.close()
-    return labels, texts
-
-
-# In[5]:
-
-## import template info
 template_text = {}
-f = open('Template_withText_Parsed_out_61template.csv', encoding="utf8")
+f = open('Template_withText_Parsed_out_template.csv', encoding="utf8")
 for line in f:
     tem = line.rstrip().split(',')
     UUID = tem[-2]
@@ -100,20 +61,13 @@ def read_file_toPrepareData_for_DualEncoder(inputfile, template_text_dic):
         split_line = line.rstrip().replace('"', '').replace('\xa0', '  ').split(",")
         cat = split_line[-1]
         text = ','.join(split_line[:len(split_line) - 1])
-        # print (cat)
-        # print (text)
-        # if cat in ['00163E3CD6251EE7B7E962A036702F73','00163E3CD6251EE7B8B4AEC37397BDF3']:
-        #   generic +=1
-        #  continue
+       
         if (cat, text) not in record:
-            # if cat != '00163E3CD6251EE7B7E962A036702F73':
+           
 
             if cat in template_text_dic:
                 record.add((cat, text))
-                # if cat in ['00163E3CD6251EE7B7E962A036702F73','00163E3CD6251EE7B8B4AEC37397BDF3']:
-                #   cat = 'generic'
-                # else:
-                #   cat = 'specific'
+                
                 texts.append(text)
                 labels.append(cat)
 
@@ -125,8 +79,7 @@ def read_file_toPrepareData_for_DualEncoder(inputfile, template_text_dic):
     print(k - 1)
     print('skip duplicated lines')
     print(skip)
-    # print ('skip generic lines')
-    # print (generic)
+    
     print('missing template content lines')
     print(missing)
 
@@ -136,7 +89,7 @@ def read_file_toPrepareData_for_DualEncoder(inputfile, template_text_dic):
 
 
 label, text = read_file_toPrepareData_for_DualEncoder(
-    'Filtered_IncomingEmailText_with_correspondingTemplateUsed_InteractionNumber1and0only.csv', template_text)
+    'Filtered.csv', template_text)
 len(label)
 len(text)
 
@@ -172,7 +125,6 @@ print(indices[:10])
 np.random.shuffle(indices)
 print(indices[:10])
 texts_shuffled = [texts[i] for i in indices]
-# L1_label = [L1_label[i] for i in indices]
 label_shuffled = [label[i] for i in indices]
 
 
@@ -202,11 +154,6 @@ import csv
 
 def clean_str(text):
     text = text.encode('ascii', 'ignore').decode('utf-8')
-    # print (text)
-    # text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
-    # text = re.sub(r'http\S+', '', text)
-    # print (text)
-    # cleanString = re.sub('[^A-Za-z0-9]+', ' ', text)
     cleanString = re.sub('\W+', ' ', text)
     return cleanString
 
@@ -215,15 +162,12 @@ def write_file_for_DualEncoder(email_text, template_text, template_dic, mode):
     ## email_text is email, template_text is corrresponding used template ID for each email, template_dic is the dic that { templateID: templatetext}
     template_collection = [clean_str(v) for k, v in template_dic.items()]
     f = unicodecsv.writer(open('%s.csv' % mode, 'wb'), encoding='utf-8')
-    # f= open('%s.csv'%mode,'w',encoding="utf8")
     if mode == 'train':
         header = ["Context", "Utterance", "Label"]
-        # f.write('Context,Utterance,Label\n')
     else:
         header = ["Context", "Ground Truth Utterance"]
         header.extend(map(lambda x: "Distractor_{}".format(x), range(len(template_collection) - 1)))
     f.writerow(header)
-    # f.writerow('Context,Ground Truth Utterance,Distractor_0,Distractor_1,Distractor_2,Distractor_3,Distractor_4,Distractor_5,Distractor_6,Distractor_7,Distractor_8\n')
     assert len(email_text) == len(template_text)
     for i in range(len(email_text)):
         Context = clean_str(email_text[i]).replace('#', '')
@@ -231,25 +175,24 @@ def write_file_for_DualEncoder(email_text, template_text, template_dic, mode):
         template_other = [clean_str(v) for k, v in template_dic.items() if k != template_text[i]]
         print('context is : \n %s \n\n template is : \n %s' % (Context, Utterance))
         if mode == 'train':
-            # f.write('"%s","%s",%s\n'%(Context,Utterance,str(1)))
+
             row_positive = [Context, Utterance, str(1)]
             f.writerow(row_positive)
             negative_template = clean_str(random.choice(template_other))
             row_negative = [Context, negative_template, str(0)]
-            # print ('negative sampled template is %s \n'%negative_template)
+           
             f.writerow(row_negative)
-            # f.write('"%s","%s",%s\n'%(Context,negative_template,str(0)))
+          
         if mode in ['val', 'test']:
             assert len(template_collection) - 1 == len(template_other)
-            # negative_templates = [clean_str(s) for s in random.sample(template_other)]
+           
             row = [Context, Utterance]
             print(row)
             row.extend(template_other)
             f.writerow(row)
 
-            # f.write('"%s","%s",%s\n'%(Context,Utterance,','.join(negative_template))
+     
 
-            # f.write()
     f.close()
 
 
